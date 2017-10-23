d8cd52dcea4f89ba7e1a8bae0d2f80f32e9b96f2
e342-2014 <e342-2014@P04-23>
2017-10-23 10:06:41 +0200

    Client-Server

---
+++ Projekat/Server/Server.cpp
@@ -0,0 +1,131 @@
+#include <winsock2.h>
+#include <stdio.h>
+
+#define SERVER_PORT 15000
+#define SERVER_SLEEP_TIME 100
+#define ACCESS_BUFFER_SIZE 1024
+#define IP_ADDRESS_LEN 16
+
+// Initializes WinSock2 library
+// Returns true if succeeded, false otherwise.
+bool InitializeWindowsSockets();
+
+int main(int argc, char* argv[])
+{
+	// Server address
+	sockaddr_in serverAddress;
+	// Server's socket
+	int serverPort = SERVER_PORT;
+	// size of sockaddr structure
+	int sockAddrLen = sizeof(struct sockaddr);
+	// buffer we will use to receive client message
+	char accessBuffer[ACCESS_BUFFER_SIZE];
+	// variable used to store function return value
+	int iResult;
+
+	if (InitializeWindowsSockets() == false)
+	{
+		// we won't log anything since it will be logged
+		// by InitializeWindowsSockets() function
+		return 1;
+	}
+
+	// Initialize serverAddress structure used by bind
+	memset((char*)&serverAddress, 0, sizeof(serverAddress));
+	serverAddress.sin_family = AF_INET; /*set server address protocol family*/
+	serverAddress.sin_addr.s_addr = INADDR_ANY;
+	serverAddress.sin_port = htons(serverPort);
+
+	// create a socket
+	SOCKET serverSocket = socket(AF_INET,      // IPv4 address famly
+		SOCK_DGRAM,   // datagram socket
+		IPPROTO_UDP); // UDP
+
+					  // check if socket creation succeeded
+	if (serverSocket == INVALID_SOCKET)
+	{
+		printf("Creating socket failed with error: %d\n", WSAGetLastError());
+		WSACleanup();
+		return 1;
+	}
+
+	// Bind port number and local address to socket
+	iResult = bind(serverSocket, (LPSOCKADDR)&serverAddress, sizeof(serverAddress));
+
+	if (iResult == SOCKET_ERROR)
+	{
+		printf("Socket bind failed with error: %d\n", WSAGetLastError());
+		closesocket(serverSocket);
+		WSACleanup();
+		return 1;
+	}
+
+	printf("Simple UDP server started and waiting client messages.\n");
+
+	// Main server loop
+	while (1)
+	{
+		// clientAddress will be set from recvfrom
+		sockaddr_in clientAddress;
+		memset(&clientAddress, 0, sizeof(sockaddr_in));
+
+		// set whole buffer to zero
+		memset(accessBuffer, 0, ACCESS_BUFFER_SIZE);
+
+		// receive client message
+		iResult = recvfrom(serverSocket,
+			accessBuffer,
+			ACCESS_BUFFER_SIZE,
+			0,
+			(LPSOCKADDR)&clientAddress,
+			&sockAddrLen);
+
+		if (iResult == SOCKET_ERROR)
+		{
+			printf("recvfrom failed with error: %d\n", WSAGetLastError());
+			continue;
+		}
+
+		char ipAddress[IP_ADDRESS_LEN];
+		// copy client ip to local char[]
+		strcpy_s(ipAddress, sizeof(ipAddress), inet_ntoa(clientAddress.sin_addr));
+		// convert port number from TCP/IP byte order to
+		// little endian byte order
+		int clientPort = ntohs((u_short)clientAddress.sin_port);
+
+		printf("Client connected from ip: %s, port: %d, sent: %s.\n", ipAddress, clientPort, accessBuffer);
+
+		// possible message processing logic could be placed here
+	}
+
+	// if we are here, it means that server is shutting down
+	// close socket and unintialize WinSock2 library
+	iResult = closesocket(serverSocket);
+	if (iResult == SOCKET_ERROR)
+	{
+		printf("closesocket failed with error: %ld\n", WSAGetLastError());
+		return 1;
+	}
+
+	iResult = WSACleanup();
+	if (iResult == SOCKET_ERROR)
+	{
+		printf("WSACleanup failed with error: %ld\n", WSAGetLastError());
+		return 1;
+	}
+
+	printf("Server successfully shut down.\n");
+	return 0;
+}
+
+bool InitializeWindowsSockets()
+{
+	WSADATA wsaData;
+	// Initialize windows sockets library for this process
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+	{
+		printf("WSAStartup failed with error: %d\n", WSAGetLastError());
+		return false;
+	}
+	return true;
+}
